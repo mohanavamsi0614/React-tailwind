79178da1dce6348181b7ff2cd12a9b4d8c693fdf
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-02-21 09:56:48 +0530

    add

--- src/Body.jsx
+++ src/Body.jsx
@@ -3,11 +3,10 @@ import Alert from '@mui/material/Alert';
 
 function Body() {
     return(
-        <div className=" flex justify-center m-8">
-        <Stack spacing={2}>
-            <button className= " bg-blue-500 text-white h-12 w-40 rounded-xl ">Button-1</button>
-            <Alert severity='error' sx={{width:"250px"}}>Alert! This is amazing</Alert>
-            <Card>
+        <div className="m-8 flex flex-col justify-between">
+            <div><button className= " bg-blue-500 text-white h-12 w-40 rounded-xl m-10">Button-1</button></div>
+            <div><Alert severity='error' sx={{width:"99%"}}>Alert! This is amazing</Alert></div>
+            <Card sx={{width:"300px",display:"flex",justifyContent:"center"}} className=' absolute left-1/3 top-80'>
             <div className='flex p-8'>
                 <img src="https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png" className='w-20'/>
                 <div className=' relative left-3'>
@@ -16,7 +15,6 @@ function Body() {
                 </div>
             </div>
             </Card>
-            </Stack>
         </div>
     )
     
